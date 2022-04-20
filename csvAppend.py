cdf4ec11927153a57528b580452df82e852c4e9a
Brock <99046455+FuriousBH@users.noreply.github.com>
2022-04-20 17:47:44 -0500

    Delete csvAppend.py

--- csvAppend.py
+++
@@ -1,8 +0,0 @@
-#-------- append PersonID to oura data csv --------
-
-import csv
-import pandas as pd
-
-df = pd.read_csv("oura-data.csv")
-df.insert(loc=0, column="PersonID", value = "00001")
-df.to_csv("oura-data.csv", index = False)
